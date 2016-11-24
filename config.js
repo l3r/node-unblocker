4c31c5cc22fe7caa852a6b62884b0ec3e737f854
l3r <sergey.s@myco.com>
2016-11-24 16:16:24 +0200

    merge config

--- config.js
+++ config.js
@@ -10,18 +10,13 @@ exports.port = process.env.PORT || 8080;
 exports.google_analytics_id = process.env.GA_ID || null;
 
 // this is used connect to keep session cookies secure. You should change this.
-<<<<<<< HEAD
+
 exports.secret = process.env.SECRET || "superduperpar01";
 
 // for redis if you're on heroku or using redis-to-go
 exports.redistogo_url = process.env.REDISTOGO_URL || "redis://redistogo:ec0a254eaeb62416ba08ad727c71974c@crestfish.redistogo.com:10567/";
 
-=======
-exports.secret = process.env.SECRET || "superdupersecretword";
 
-// for redis if you're on heroku or using redis-to-go
-exports.redistogo_url = process.env.REDISTOGO_URL || "redis://redistogo:3a81328dd3933b83358c0bc945e13d06@hammerjaw.redistogo.com:11307/";
->>>>>>> 85e1f0779be64d1991b7053147b55229ccd45a74
 
 // for redis if you're running your own copy
 exports.redis_host = "localhost";
